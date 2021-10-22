2a4c1e730770a008c391a8f2533e20d25bcd742f
steph45607 <staniswinata.stephanie@gmail.com>
2021-10-22 10:44:12 +0700

    full logo

--- Turtle/logo.py
+++ Turtle/logo.py
@@ -10,13 +10,14 @@ def curvel(num):
         wrp.forward(0.4)
 
 wrp = turtle.Turtle()
-wrp.speed(0)
+wrp.speed(1000)
 
 #set mid position
 wrp.penup()
 wrp.setposition(-280,0)
 wrp.pendown()
 
+#set red part
 wrp.color("#DB4437")
 wrp.begin_fill()
 wrp.setheading(0)
@@ -27,9 +28,7 @@ wrp.forward(80)
 curvel(45)
 wrp.end_fill()
 
-# wrp.penup()
-# wrp.setposition(-280,0)
-# wrp.pendown()
+#set blue part
 wrp.color("#4285F4")
 wrp.begin_fill()
 curvel(80)
@@ -37,27 +36,37 @@ wrp.forward(80)
 curvel(180)
 wrp.forward(80)
 curvel(90)
-
-
-# wrp.setheading(180)
-# wrp.right(15)
-# #start middle bottom pill
-# wrp.right(15)
-# wrp.forward(40)
-# curvel(180)
-# wrp.forward(80)
-# curvel(180)
-# wrp.forward(40)
 wrp.end_fill()
-# curve(360)
-# wrp.forward(40)
 
-# wrp.setheading(180)
-# wrp.right(15)
-# wrp.forward(40)
-# curvel(180)
-# wrp.forward(80)
-# curve(90)
+#set to initial position
+wrp.penup()
+wrp.setposition(-280,0)
+wrp.left(115)
+wrp.forward(150)
+wrp.pendown()
+
+#make the >
+#make yellow part
+wrp.color("#F4B400")
+wrp.begin_fill()
+wrp.right(150)
+wrp.forward(40)
+curvel(180)
+wrp.forward(80)
+curvel(45)
+wrp.end_fill()
+
+#green part
+wrp.color("#0F9D54")
+wrp.begin_fill()
+curvel(80)
+wrp.forward(80)
+curvel(180)
+wrp.forward(80)
+curvel(90)
+wrp.end_fill()
+
+
 
 
 turtle.done()
